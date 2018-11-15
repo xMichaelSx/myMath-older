9ef027f8f6f825834457d49f6ccf8b34f975ac69
amitai <amitaymizrahy@gmail.com>
2018-11-15 18:56:07 +0000

    deledted de.erichseifert.gral.plots.points.PointRenderer from most of the classes and replace readme in readme for assinment 1

--- src/myMath/Test.java
+++ src/myMath/Test.java
@@ -1,7 +1,7 @@
 package myMath;
 public class Test {
 	public static void main(String[] args) {
-Polynom p=new Polynom("0.2x^4-1.5x^3+3.0x^2-x-5");
+Polynom p=new Polynom("13.4x^4-10x^4");
 		p.graf();
 		
  
